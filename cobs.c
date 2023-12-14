4c6c36cbce7b5213844392f6658d337cfd465a45
crosstyan <crosstyan@outlook.com>
2023-12-14 12:50:15 +0800

    change API for fun

--- cobs.c
+++ cobs.c
@@ -6,36 +6,44 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include "cobs.h"
 
-/* Stuffs "length" bytes of data at the location pointed to by
- * "input", writing the output to the location pointed to by
- * "output". Returns the number of bytes written to "output".
- *
- * Remove the "restrict" qualifiers if compiling with a
- * pre-C99 C dialect.
+#define COBS_DELIMITER 0x00
+
+/**
+ * @brief encode the input buffer to output buffer
+ * @param [in]input input buffer
+ * @param [in]length input buffer length
+ * @param [out]output output buffer
+ * @param [in,out]out_length output buffer length
+ * @return error code, see enum COBS_ERRORS
  */
-size_t cobs_encode(const uint8_t * restrict input, size_t length, uint8_t * restrict output)
-{
+enum COBS_ERRORS cobs_encode(const uint8_t *input, size_t length, uint8_t *output, size_t *out_length) {
     size_t read_index = 0;
     size_t write_index = 1;
     size_t code_index = 0;
     uint8_t code = 1;
 
-    while(read_index < length)
-    {
-        if(input[read_index] == 0)
-        {
+    if (out_length == NULL) {
+        return COBS_ERR_OUT_BUFFER_NULL;
+    }
+
+    // at most one byte for every 254 non-zero bytes
+    // plus one byte for the end-of-frame marker.
+    if (*out_length < length + (length / 254) + 2) {
+        return COBS_ERR_OUT_BUFFER_TOO_SMALL;
+    }
+
+    while (read_index < length) {
+        if (input[read_index] == 0) {
             output[code_index] = code;
             code = 1;
             code_index = write_index++;
             read_index++;
-        }
-        else
-        {
+        } else {
             output[write_index++] = input[read_index++];
             code++;
-            if(code == 0xFF)
-            {
+            if (code == 0xFF) {
                 output[code_index] = code;
                 code = 1;
                 code_index = write_index++;
@@ -44,8 +52,10 @@ size_t cobs_encode(const uint8_t * restrict input, size_t length, uint8_t * rest
     }
 
     output[code_index] = code;
+    output[code_index + 1] = COBS_DELIMITER;
 
-    return write_index;
+    *out_length = write_index;
+    return COBS_ERR_SUCCESS;
 }
 
 /* Unstuffs "length" bytes of data at the location pointed to by
@@ -57,33 +67,50 @@ size_t cobs_encode(const uint8_t * restrict input, size_t length, uint8_t * rest
  * Remove the "restrict" qualifiers if compiling with a
  * pre-C99 C dialect.
  */
-size_t cobs_decode(const uint8_t * restrict input, size_t length, uint8_t * restrict output)
-{
+
+/**
+ * @brief decode the input buffer to output buffer
+ * @param [in]input input buffer
+ * @param [in]length input buffer length
+ * @param [out]output output buffer
+ * @param [in,out]out_length output buffer length
+ * @return error code, see enum COBS_ERRORS
+ */
+enum COBS_ERRORS cobs_decode(const uint8_t *input, size_t length, uint8_t *output, size_t *out_length) {
     size_t read_index = 0;
     size_t write_index = 0;
     uint8_t code;
     uint8_t i;
 
-    while(read_index < length)
-    {
+    if (out_length == NULL) {
+        return COBS_ERR_OUT_BUFFER_NULL;
+    }
+
+    if (*out_length < (length - 1)) {
+        return COBS_ERR_OUT_BUFFER_TOO_SMALL;
+    }
+
+    while (read_index < length) {
         code = input[read_index];
 
-        if(read_index + code > length && code != 1)
-        {
-            return 0;
+        if (code == COBS_DELIMITER) {
+            return COBS_ERR_ZERO_BYTE_IN_INPUT;
+        }
+
+        if (read_index + code > length && code != 1) {
+            return COBS_ERR_ZERO_BEYOND_BUFFER;
         }
 
         read_index++;
 
-        for(i = 1; i < code; i++)
-        {
+        for (i = 1; i < code; i++) {
             output[write_index++] = input[read_index++];
         }
-        if(code != 0xFF && read_index != length)
-        {
-            output[write_index++] = '\0';
+        if (code != 0xFF && read_index != length) {
+            output[write_index++] = 0x00;
         }
     }
 
-    return write_index;
+    *out_length = write_index;
+    return COBS_ERR_SUCCESS;
 }
--- cobs.h
+++ cobs.h
@@ -14,8 +14,18 @@
 extern "C" {
 #endif
 
-size_t cobs_encode(const uint8_t *input, size_t length, uint8_t *output);
-size_t cobs_decode(const uint8_t *input, size_t length, uint8_t *output);
+enum COBS_ERRORS
+{
+    COBS_ERR_SUCCESS = 0,
+    COBS_ERR_OUT_BUFFER_TOO_SMALL = -1,
+    COBS_ERR_ZERO_BYTE_IN_INPUT = -2,
+    COBS_ERR_OUT_BUFFER_NULL = -4,
+    COBS_ERR_ZERO_BEYOND_BUFFER = -5,
+};
+
+
+enum COBS_ERRORS cobs_encode(const uint8_t *input, size_t length, uint8_t *output, size_t *out_length);
+enum COBS_ERRORS cobs_decode(const uint8_t *input, size_t length, uint8_t *output, size_t *out_length);
 
 #ifdef __cplusplus
 }
